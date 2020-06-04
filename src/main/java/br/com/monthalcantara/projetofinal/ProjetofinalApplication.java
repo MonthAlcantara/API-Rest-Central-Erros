cad89fa4a4909edcdb129dc5476c1b61e49772a2
Montival Junior <montival_junior@yahoo.com.br>
2020-06-04 15:24:10 -0300

    Primeiro commit

---
+++ src/main/java/br/com/monthalcantara/projetofinal/ProjetofinalApplication.java
@@ -0,0 +1,13 @@
+package br.com.monthalcantara.projetofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/monthalcantara/projetofinal/ProjetofinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.monthalcantara.projetofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
