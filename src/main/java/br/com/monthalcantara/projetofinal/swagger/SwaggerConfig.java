cb30150d4dbcd4ef52af180eb3cbd257c7b71211
Montival Junior <montival_junior@yahoo.com.br>
2020-06-17 12:50:11 -0300

    Alterada dependencia do Swagger e adicionada classe segurança

---
+++ src/main/java/br/com/monthalcantara/projetofinal/swagger/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package br.com.monthalcantara.projetofinal.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket apis() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.monthalcantara.projetofinal"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Central de Erros",
+                "Desafio final AceleraDev Java",
+                "1.0",
+                "Termos de serviço",
+                null,
+                "Licença",
+                "URL da licença",
+                new ArrayList<>()
+        );
+
+    }
+}
