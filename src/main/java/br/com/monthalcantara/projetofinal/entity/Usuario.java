aaf4148d38a90b6e452749d7a3cae5fab639c30e
Montival Junior <montival_junior@yahoo.com.br>
2020-06-17 02:17:49 -0300

    Adicionada entidade Usuário

---
+++ src/main/java/br/com/monthalcantara/projetofinal/entity/Usuario.java
@@ -0,0 +1,27 @@
+package br.com.monthalcantara.projetofinal.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String login;
+
+    private String password;
+}
