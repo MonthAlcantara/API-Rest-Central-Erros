87c62033cfc51bd8fda30c91d674b57ec164bd0a
Montival Junior <montival_junior@yahoo.com.br>
2020-06-20 16:27:32 -0300

    Adicionadas descrições aos métodos do UsuarioController

--- src/main/java/br/com/monthalcantara/projetofinal/endpoint/UsuarioController.java
+++ src/main/java/br/com/monthalcantara/projetofinal/endpoint/UsuarioController.java
@@ -2,6 +2,7 @@ package br.com.monthalcantara.projetofinal.endpoint;
 
 import br.com.monthalcantara.projetofinal.entity.Usuario;
 import br.com.monthalcantara.projetofinal.service.interfaces.UsuarioService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,27 +20,32 @@ public class UsuarioController {
     private UsuarioService userService;
 
     @GetMapping
-    public ResponseEntity<List<Usuario>> getById() {
+    @ApiOperation("Busca todos os Usuários")
+    public ResponseEntity<List<Usuario>> findAll() {
         return new ResponseEntity<>(this.userService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Busca um usuário pelo Id")
     public ResponseEntity<Usuario> getById(@PathVariable("id") Long id) {
         return new ResponseEntity<>(this.userService.findById(id).get(), HttpStatus.OK);
     }
 
     @PostMapping
+    @ApiOperation("Cria um novo Usuário")
     public ResponseEntity<Usuario> create(@Valid @RequestBody Usuario usuario) {
         return new ResponseEntity<>(this.userService.save(usuario), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation("Exclui um Usuário")
     public void deleteById(@PathVariable("id") Long id) {
         this.userService.deleteById(id);
     }
 
 
     @PutMapping
+    @ApiOperation("Atualiza um Usuário")
     public ResponseEntity<Usuario> update(@Valid @RequestBody Usuario usuario) {
         return new ResponseEntity<>(this.userService.save(usuario), HttpStatus.ACCEPTED);
     }
