046af5629aa71ab1def989cdfa71e88f911e5174
Montival Junior <montival_junior@yahoo.com.br>
2020-06-21 13:45:49 -0300

    Implementado UsuarioDTO

--- src/main/java/br/com/monthalcantara/projetofinal/dto/UsuarioDTO.java
+++ src/main/java/br/com/monthalcantara/projetofinal/dto/UsuarioDTO.java
@@ -1,4 +1,26 @@
 package br.com.monthalcantara.projetofinal.dto;
 
+import br.com.monthalcantara.projetofinal.entity.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
+
+    private String login;
+    private String password;
+
+    public Usuario build(){
+        return new Usuario()
+                .setLogin(this.login)
+                .setPassword(this.password);
+    }
+
+    public UsuarioDTO(Usuario usuario){
+        this.login = usuario.getLogin();
+    }
+
 }
--- src/main/java/br/com/monthalcantara/projetofinal/entity/Usuario.java
+++ src/main/java/br/com/monthalcantara/projetofinal/entity/Usuario.java
@@ -3,6 +3,7 @@ package br.com.monthalcantara.projetofinal.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +16,7 @@ import java.util.Collection;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Accessors(chain = true)
 @Entity
 @Table(name = "users")
 public class Usuario implements UserDetails {
