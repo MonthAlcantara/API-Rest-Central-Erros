a5a99a249f4c51783b08dd8457db370d215b47f4
Montival Junior <montival_junior@yahoo.com.br>
2020-06-23 12:57:58 -0300

    Eliminadado UserDetails ao UsuarioDTO

--- src/main/java/br/com/monthalcantara/projetofinal/dto/UsuarioDTO.java
+++ src/main/java/br/com/monthalcantara/projetofinal/dto/UsuarioDTO.java
@@ -4,15 +4,11 @@ import br.com.monthalcantara.projetofinal.entity.Usuario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UsuarioDTO implements UserDetails {
+public class UsuarioDTO{
 
     private String login;
     private String password;
@@ -26,40 +22,5 @@ public class UsuarioDTO implements UserDetails {
     public UsuarioDTO(Usuario usuario){
         this.login = usuario.getLogin();
         this.password = usuario.getPassword();
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.login;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 }
