ddc15d20a42716a4414a553279ff772877dd6f3a
Montival Junior <montival_junior@yahoo.com.br>
2020-06-24 03:54:20 -0300

    Atualização segurança config

--- src/main/java/br/com/monthalcantara/projetofinal/security/SegurancaConfig.java
+++ src/main/java/br/com/monthalcantara/projetofinal/security/SegurancaConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -34,7 +35,7 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/h2/**",
                 "/v2/api-docs",
-                "/configuration/ui",
+                "/configuration/**",
                 "/swagger-resources/**",
                 "/configuration/security",
                 "/swagger-ui.html",
@@ -53,5 +54,16 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
-
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // TODO Auto-generated method stub
+        http.authorizeRequests()
+                .antMatchers("/v2/api-docs", "/swagger-resources/configuration/ui", "/swagger-resources", "/swagger-resources/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable();
+    }
 }
