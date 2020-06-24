f89621ed5802c26118550e7104f1124692c060a0
Montival Junior <montival_junior@yahoo.com.br>
2020-06-24 01:57:08 -0300

    Implementado PersistenceConfig

--- src/main/java/br/com/monthalcantara/projetofinal/configuration/PersistenceConfig.java
+++ src/main/java/br/com/monthalcantara/projetofinal/configuration/PersistenceConfig.java
@@ -2,13 +2,12 @@ package br.com.monthalcantara.projetofinal.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaAuditing( auditorAwareRef =  "auditorAware" )
+//@EnableJpaAuditing( auditorAwareRef =  "auditorAware" )
 public class PersistenceConfig {
 
     @Bean
