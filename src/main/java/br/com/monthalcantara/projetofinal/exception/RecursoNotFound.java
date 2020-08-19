b3896d902474b5cb1f35d2be7a93f1daf5fd5a53
Montival <montival_junior@yahoo.com.br>
2020-08-19 12:36:07 -0300

    Adicionado nova exception para tratar not found

---
+++ src/main/java/br/com/monthalcantara/projetofinal/exception/RecursoNotFound.java
@@ -0,0 +1,7 @@
+package br.com.monthalcantara.projetofinal.exception;
+
+public class RecursoNotFound extends RuntimeException {
+    public RecursoNotFound(String message) {
+        super(message);
+    }
+}
