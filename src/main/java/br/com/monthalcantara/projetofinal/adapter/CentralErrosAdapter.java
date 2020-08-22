7a0e62a489739852467fbdac27e28f5bae20d658
Montival <montival_junior@yahoo.com.br>
2020-08-22 05:47:40 -0300

    Adicionada classe para tratamento de pageable

---
+++ src/main/java/br/com/monthalcantara/projetofinal/adapter/CentralErrosAdapter.java
@@ -0,0 +1,20 @@
+package br.com.monthalcantara.projetofinal.adapter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+//@Configuration
+public class CentralErrosAdapter implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver phmar = new PageableHandlerMethodArgumentResolver();
+        phmar.setFallbackPageable(PageRequest.of(0, 5));
+        resolvers.add(phmar);
+    }
+}
