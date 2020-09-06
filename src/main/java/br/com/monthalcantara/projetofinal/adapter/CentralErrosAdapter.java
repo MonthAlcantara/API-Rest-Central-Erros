1bd957a273e8479cd11b179f33fad27a5dcef6ce
Montival <montival_junior@yahoo.com.br>
2020-09-06 01:28:35 -0300

    refatoracao

--- src/main/java/br/com/monthalcantara/projetofinal/adapter/CentralErrosAdapter.java
+++ src/main/java/br/com/monthalcantara/projetofinal/adapter/CentralErrosAdapter.java
@@ -1,6 +1,5 @@
 package br.com.monthalcantara.projetofinal.adapter;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -8,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-//@Configuration
 public class CentralErrosAdapter implements WebMvcConfigurer {
 
     @Override
