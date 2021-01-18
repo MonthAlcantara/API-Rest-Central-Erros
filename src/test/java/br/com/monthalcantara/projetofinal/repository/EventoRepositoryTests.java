39c667a1edcafcac011371ca3cfe92515753c8a9
monthalcantara <montival_junior@yahoo.com.br>
2021-01-17 21:15:33 -0300

    Realizado ajuste nos testes do repository

--- src/test/java/br/com/monthalcantara/projetofinal/repository/EventoRepositoryTests.java
+++ src/test/java/br/com/monthalcantara/projetofinal/repository/EventoRepositoryTests.java
@@ -3,29 +3,33 @@ package br.com.monthalcantara.projetofinal.repository;
 import br.com.monthalcantara.projetofinal.enums.Level;
 import br.com.monthalcantara.projetofinal.model.Evento;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 
 
-@SpringBootTest
-@ActiveProfiles("test")
+@DataJpaTest
 public class EventoRepositoryTests {
-    Evento evento, eventoSalvo;
+    private Evento evento, eventoSalvo;
 
     @Autowired
-    EventoRepository eventoRepository;
+    private EventoRepository eventoRepository;
 
+    @BeforeEach
+    public void init(){
+        evento = geradorDeEvento();
+        eventoSalvo = eventoRepository.save(evento);
+    }
 
     @Test
     @DisplayName("Deve salvar um novo evento no banco de dados")
     void deveCriarEvento() {
-        evento = geradorDeEvento();
-        eventoSalvo = eventoRepository.save(evento);
 
         Assertions.assertThat(eventoSalvo).isNotNull();
     }
@@ -33,37 +37,33 @@ public class EventoRepositoryTests {
     @Test
     @DisplayName("Deve buscar um evento salvo pelo Id")
     void deveBuscarPeloId() {
-        evento = geradorDeEvento();
-        eventoSalvo = eventoRepository.save(evento);
         Assertions.assertThat(eventoRepository.findById(eventoSalvo.getId())).isPresent();
     }
 
     @Test
     @DisplayName("Não deve buscar evento inexistente")
     void naoDeveBuscarEventoInexistente() {
-        Assertions.assertThat(eventoRepository.findById(1L)).isNotPresent();
+        Assertions.assertThat(eventoRepository.findById(50L)).isNotPresent();
     }
 
     @Test
     @DisplayName("Deve deletar um evento")
     void deveDeletarUmEvento() {
-        evento = geradorDeEvento();
-        eventoRepository.save(evento);
-        eventoRepository.deleteById(1l);
-        Assertions.assertThat(eventoRepository.findById(1L)).isNotPresent();
+        eventoRepository.deleteById(eventoSalvo.getId());
+        Assertions.assertThat(eventoRepository.findById(eventoSalvo.getId())).isNotPresent();
     }
 
     @Test
     @DisplayName("Deve atualizar um evento")
     void deveAtualizarEvento() {
-        evento = geradorDeEvento();
-        eventoSalvo = eventoRepository.save(evento);
+
         eventoSalvo.setDescricao("Teste2");
         eventoSalvo.setLevel(Level.INFO);
         eventoSalvo.setLog("Teste2");
+
         eventoSalvo = eventoRepository.save(eventoSalvo);
+
         Assertions.assertThat(eventoSalvo.getDescricao()).isNotEqualTo(evento.getDescricao());
-        Assertions.assertThat(eventoSalvo.getId()).isEqualTo(evento.getId());
         Assertions.assertThat(eventoSalvo.getLevel()).isNotEqualTo(evento.getLevel());
         Assertions.assertThat(eventoSalvo.getLog()).isNotEqualTo(evento.getLog());
 
